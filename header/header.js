b6c3b35ba601849ec82c18b8402e15accd625d35
icheff <icheff.com@gmail.com>
2024-01-17 00:54:17 +0100

    Update

--- header/header.js
+++ header/header.js
@@ -1,5 +1,5 @@
 // header.js
 fetch('header/header.html')
   .then(response => response.ok ? response.text() : Promise.reject('El archivo HTML del footer no se encontró.'))
-  .then(footerData => document.getElementById('footerContainer').innerHTML = footerData)
+  .then(footerData => document.getElementById('headerContainer').innerHTML = footerData)
   .catch(error => console.error(error.message));
